2055009bfb4331dd38cd9513207928b1cbad82ef
howell <howell@tianxingaoyideMacBook-Pro.local>
2021-07-31 15:07:20 +0800

    feat: 更新测试

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(async ctx => {
-  ctx.body = 'Hello World';
+  ctx.body = '这是第一个nodejs程序部署到阿里云！！！';
 });
 
 app.listen(3000,'0.0.0.0',function () {
-    console.log('服务器启动成功');
+    console.log('服务器启动成功!监听端口：3000');
 });
