84f0837cb4735f9e263a12f797a898db599cbc5d
howell <howell@tianxingaoyideMacBook-Pro.local>
2021-07-31 15:16:57 +0800

    feat: 更新测试

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.use(async ctx => {
   ctx.body = '这是第一个nodejs程序部署到阿里云！！！';
 });
 
-app.listen(3000,'0.0.0.0',function () {
-    console.log('服务器启动成功!监听端口：3000');
+app.listen(80,'0.0.0.0',function () {
+    console.log('服务器启动成功!监听端口：80');
 });
